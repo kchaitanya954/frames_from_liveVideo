cbd06c68706eaffcb032c49edccf651e0038b88b
Krishna Chaitanya Pasala <67577627+kchaitanya954@users.noreply.github.com>
2021-04-26 23:16:02 +0530

    Update genVideo.py

--- test/genVideo.py
+++ test/genVideo.py
@@ -3,13 +3,13 @@ import numpy as np
 import glob
 
 img_array = []
-for filename in glob.glob('/home/chaitanya/Documents/Bid data/project/test/*.jpg'):
+for filename in sorted(glob.glob('/home/chaitanya/Documents/Bid data/project/test/images/*.jpg')):
     img = cv2.imread(filename)
     height, width, layers = img.shape
     size = (width, height)
     img_array.append(img)
 
-out = cv2.VideoWriter('pk.avi', cv2.VideoWriter_fourcc(*'DIVX'), 3, size)
+out = cv2.VideoWriter('pk.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
 
 for i in range(len(img_array)):
     out.write(img_array[i])
